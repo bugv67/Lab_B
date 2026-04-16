cc342901ca51090d98a2ae9023f02d19875833a8
Noa Weiss <noa67weiss@gmail.com>
2026-04-16 23:02:23 +0300

    1b list

--- AntiVirus.c
+++ AntiVirus.c
@@ -8,6 +8,12 @@ typedef struct virus
     unsigned char *VirusName;
     unsigned char *Sig;
 } virus;
+typedef struct link
+{
+    link *nextVirus; // next
+    virus *vir;      // data
+} link;
+
 virus *readVirus(FILE *file)
 // Input: file pointer and returns a virus* that represents the next virus in the file.
 //  To read from a file, use fread(). See man fread(3) for assistance.
@@ -62,6 +68,44 @@ void printVirus(virus *virus, FILE *output)
     }
     fprintf(output, "\n");
 }
+
+void list_print(link *virus_list, FILE *)
+/* Print the data of every link in list to the given stream. Each item followed by a newline character. */
+{
+    for (link *current = virus_list; current != NULL; current = current->nextVirus)
+    {
+        printVirus(current->vir, stdout);
+    }
+}
+link *list_append(link *virus_list, virus *data)
+/* Add a new link with the given data to the list (at the end CAN ALSO AT BEGINNING)
+//return a pointer to the list (i.e., the first link in the list). If the list is null - create a new entry and return a pointer to the entry. */
+{
+    link *newLink = malloc(sizeof(link));
+    if (newLink == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return virus_list;
+    }
+    newLink->vir = data;
+    newLink->nextVirus = virus_list;
+    return newLink;
+}
+void list_free(link *virus_list)
+/* Free the memory allocated by the list. */
+{
+    link *current = virus_list;
+    while (current != NULL)
+    {
+        link *next = current->nextVirus;
+        free(current->vir->VirusName); // free th data
+        free(current->vir->Sig);
+        free(current->vir); // free the virus struct
+        free(current);      // free the link
+        current = next;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     if (argc > 1)
